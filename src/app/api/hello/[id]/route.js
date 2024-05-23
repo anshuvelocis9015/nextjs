643ebc6673a6d00af66b458ff2261d3adf705cd6
anshu9015 <anshu.kmr9015@gmail.com>
2024-05-23 23:58:54 +0530

    put command

--- src/app/api/hello/[id]/route.js
+++ src/app/api/hello/[id]/route.js
@@ -6,3 +6,14 @@ export function GET(request,content){
     console.log(data,content.params.id);
     return NextResponse.json(data.length==0?{result:"No result found",success:false}:{result:data[0],success:true,status:200})
 }
+export async function PUT(request,content){
+    let payload = await request.json();
+    let userid = content.params.id;
+    payload.id = content.params.id;
+    console.log(userid);
+    console.log(payload.name,payload.age,payload.email,payload.id);
+    if(!payload.name || !payload.age || !payload.email || !payload.id){
+        return NextResponse.json({result:"required new field",success:false},{status:400})
+    }
+    return NextResponse.json({result:payload,success:true},{status:200})
+}
